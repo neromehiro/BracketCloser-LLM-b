fecd40c1cae661176b550d0021e5689035c80c4f
neromehiro <neromehiro1014@gmail.com>
2024-06-06 13:15:28 +0900

    hyper.pyの学習が完璧にうまくいった

--- hyper.py
+++ hyper.py
@@ -12,13 +12,10 @@ from modules.data_utils import load_dataset, prepare_sequences, tokens
 from modules.training_utils import train_model, plot_training_history, save_metadata
 import glob
 
-encode_dir_path = "./components/dataset/preprocessed/"
-model_save_base_path = "./models/"
-storage_base_path = "./optuna_studies/"
-
-model_architecture_func = None
-architecture = None
-best_loss = float('inf')
+ENCODE_DIR_PATH = "./components/dataset/preprocessed/"
+MODEL_SAVE_BASE_PATH = "./models/"
+STORAGE_BASE_PATH = "./optuna_studies/"
+BEST_LOSS = float('inf')
 
 os.environ["WANDB_CONSOLE"] = "off"
 os.environ["WANDB_SILENT"] = "true"
@@ -30,12 +27,10 @@ def clean_up_files(save_path, keep_files=['best_model.h5', 'training_history.png
             os.remove(file)
 
 def main():
-    global model_architecture_func, architecture, best_loss
-    
     option = input("Choose an option:\n1. Resume existing study\n2. Start a new study\nEnter 1 or 2: ").strip()
     
     if option == "1":
-        studies = [f for f in os.listdir(storage_base_path) if f.startswith("hyper_")]
+        studies = [f for f in os.listdir(STORAGE_BASE_PATH) if f.startswith("hyper_")]
         if not studies:
             print("No existing studies found. Starting a new study.")
             option = "2"
@@ -45,35 +40,22 @@ def main():
             study_index = int(input("Enter the number of the study to resume: ").strip()) - 1
             study_folder = studies[study_index]
             study_name = study_folder
-            architecture_name_parts = study_folder.split('_')[1:2]
-            architecture_name = architecture_name_parts[0]
-            storage_name = f"sqlite:///{storage_base_path}/{study_folder}/optuna_study.db"
+            architecture_name = study_folder.split('_')[1]
+            storage_name = f"sqlite:///{STORAGE_BASE_PATH}/{study_folder}/optuna_study.db"
             model_architecture_func, architecture = setup(architecture_name)
-            save_path = os.path.join(storage_base_path, study_folder)
+            save_path = os.path.join(STORAGE_BASE_PATH, study_folder)
 
     if option == "2":
         architecture_name = input("Enter the model architecture (gru, transformer, lstm, bert, gpt): ").strip()
         model_architecture_func, architecture = setup(architecture_name)
-
-        save_path = os.path.join(storage_base_path, "hyper_" + architecture_name)
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
+        save_path = create_save_folder(STORAGE_BASE_PATH, architecture_name)
         study_name = os.path.basename(save_path)
         storage_name = f"sqlite:///{save_path}/optuna_study.db"
-    
+
     time_limit_str = input("Enter the training time limit (e.g., '3min', '1hour', '5hour'): ").strip()
     time_limit = parse_time_limit(time_limit_str)
     start_time = datetime.now()
 
-    trial_timeout = 60
-
-    study = optuna.create_study(
-        study_name=study_name, 
-        direction="minimize", 
-        storage=storage_name, 
-        load_if_exists=True
-    )
-    
     progress_bar = tqdm(total=time_limit.total_seconds(), desc="Optimization Progress", unit="s")
 
     def callback(study, trial):
@@ -87,14 +69,17 @@ def main():
     n_jobs = int(input("Enter the number of parallel jobs: ").strip())
 
     try:
-        study.optimize(lambda trial: objective(trial, architecture, best_loss, encode_dir_path, lambda: create_save_folder(save_path, architecture)), timeout=time_limit.total_seconds(), callbacks=[callback])
+        study = optuna.create_study(
+            study_name=study_name, 
+            direction="minimize", 
+            storage=storage_name, 
+            load_if_exists=True
+        )
+        study.optimize(lambda trial: objective(trial, architecture, BEST_LOSS, ENCODE_DIR_PATH, lambda: create_save_folder(save_path, architecture)), timeout=time_limit.total_seconds(), callbacks=[callback])
     except Exception as e:
         print(f"An exception occurred during optimization: {e}")
     finally:
         progress_bar.close()
-
-    print("Best hyperparameters: ", study.best_params)
-    print("Best loss: ", study.best_value)
 
     best_params = study.best_params
     epochs = best_params["epochs"]
@@ -102,61 +87,9 @@ def main():
     learning_rate = best_params["learning_rate"]
     seq_length = 30
 
-    if architecture == "gru":
-        embedding_dim = best_params["embedding_dim"]
-        gru_units = best_params["gru_units"]
-        dropout_rate = best_params["dropout_rate"]
-        recurrent_dropout_rate = best_params["recurrent_dropout_rate"]
-        model = model_architecture_func(seq_length, len(tokens) + 1, learning_rate, embedding_dim, gru_units, dropout_rate, recurrent_dropout_rate)
+    model = create_model(architecture, best_params, model_architecture_func, seq_length, len(tokens))
 
-    elif architecture == "transformer":
-        embedding_dim = best_params["embedding_dim"]
-        num_heads = best_params["num_heads"]
-        ffn_units = best_params["ffn_units"]
-        dropout_rate = best_params["dropout_rate"]
-        model = model_architecture_func(seq_length, len(tokens) + 1, learning_rate, embedding_dim, num_heads, ffn_units, dropout_rate)
-
-    elif architecture == "lstm":
-        embedding_dim = best_params["embedding_dim"]
-        lstm_units = best_params["lstm_units"]
-        dropout_rate = best_params["dropout_rate"]
-        recurrent_dropout_rate = best_params["recurrent_dropout_rate"]
-        num_layers = best_params["num_layers"]
-        model = model_architecture_func(seq_length, len(tokens) + 1, learning_rate, embedding_dim, lstm_units, dropout_rate, recurrent_dropout_rate, num_layers)
-
-    elif architecture == "bert":
-        embedding_dim = best_params["embedding_dim"]
-        num_heads = best_params["num_heads"]
-        ffn_units = best_params["ffn_units"]
-        num_layers = best_params["num_layers"]
-        dropout_rate = best_params["dropout_rate"]
-        model = model_architecture_func(seq_length, len(tokens) + 1, learning_rate, embedding_dim, num_heads, ffn_units, num_layers, dropout_rate)
-
-    elif architecture == "gpt":
-        embedding_dim = best_params["embedding_dim"]
-        num_heads = best_params["num_heads"]
-        ffn_units = best_params["ffn_units"]
-        dropout_rate = best_params["dropout_rate"]
-        model = model_architecture_func(seq_length, len(tokens) + 1, learning_rate, embedding_dim, num_heads, ffn_units, dropout_rate)
-
-    vocab_set = set(tokens)
-    all_input_sequences = []
-    all_target_tokens = []
-
-    num_datasets = 0
-    num_files = 10
-
-    for dirpath, dirnames, filenames in os.walk(encode_dir_path):
-        for file in filenames[:num_files]:
-            file_path = os.path.join(dirpath, file)
-            encoded_tokens_list = load_dataset(file_path)
-            if encoded_tokens_list is None:
-                continue
-            for encoded_tokens in encoded_tokens_list:
-                num_datasets += 1
-                input_sequences, target_tokens = prepare_sequences(encoded_tokens, seq_length=seq_length)
-                all_input_sequences.extend(input_sequences)
-                all_target_tokens.extend(target_tokens)
+    all_input_sequences, all_target_tokens = load_training_data(ENCODE_DIR_PATH, seq_length)
 
     if not all_input_sequences or not all_target_tokens:
         print("No data for training.")
@@ -171,7 +104,7 @@ def main():
     mirrored_strategy = tf.distribute.MirroredStrategy()
 
     with mirrored_strategy.scope():
-        model = model_architecture_func(seq_length, len(tokens) + 1, embedding_dim, num_heads, ffn_units, dropout_rate, learning_rate)
+        model = create_model(architecture, best_params, model_architecture_func, seq_length, len(tokens))
 
     model_path = os.path.join(save_path, "best_model.h5")
     plot_path = os.path.join(save_path, "training_history.png")
@@ -183,14 +116,14 @@ def main():
         epochs=epochs, 
         batch_size=batch_size, 
         model_path=model_path, 
-        num_files=num_files, 
+        num_files=10, 
         learning_rate=learning_rate, 
         architecture=architecture, 
         model_architecture_func=model_architecture_func
     )
     
     if history:
-        plot_training_history(history, save_path=plot_path, epochs=epochs, batch_size=batch_size, learning_rate=learning_rate, num_files=num_files, dataset_size=dataset_size)
+        plot_training_history(history, save_path=plot_path, epochs=epochs, batch_size=batch_size, learning_rate=learning_rate, num_files=10, dataset_size=dataset_size)
 
     model_size = os.path.getsize(model_path) / (1024 * 1024)
     model_params = model.count_params()
@@ -198,7 +131,7 @@ def main():
     metadata = {
         "epochs": epochs,
         "batch_size": batch_size,
-        "num_files": num_files,
+        "num_files": 10,
         "learning_rate": learning_rate,
         "dataset_size": dataset_size,
         "model_size_MB": model_size,
@@ -207,12 +140,46 @@ def main():
     }
     save_metadata(model_path, metadata)
 
-    # Clean up unnecessary files, keeping only the latest model and training history
     clean_up_files(save_path, keep_files=['best_model.h5', 'training_history.png', 'optuna_study.db'])
 
     print(f"Training finished.")
     print(f"Model size: {model_size:.2f} MB")
     print(f"Model parameters: {model_params}")
 
+def create_model(architecture, best_params, model_architecture_func, seq_length, vocab_size):
+    embedding_dim = best_params["embedding_dim"]
+    dropout_rate = best_params["dropout_rate"]
+    learning_rate = best_params["learning_rate"]
+
+    if architecture == "gru":
+        return model_architecture_func(seq_length, vocab_size + 1, learning_rate, embedding_dim, best_params["gru_units"], dropout_rate, best_params["recurrent_dropout_rate"])
+    elif architecture == "transformer":
+        return model_architecture_func(seq_length, vocab_size + 1, learning_rate, embedding_dim, best_params["num_heads"], best_params["ffn_units"], dropout_rate)
+    elif architecture == "lstm":
+        return model_architecture_func(seq_length, vocab_size + 1, learning_rate, embedding_dim, best_params["lstm_units"], dropout_rate, best_params["recurrent_dropout_rate"], best_params["num_layers"])
+    elif architecture == "bert":
+        return model_architecture_func(seq_length, vocab_size + 1, learning_rate, embedding_dim, best_params["num_heads"], best_params["ffn_units"], best_params["num_layers"], dropout_rate)
+    elif architecture == "gpt":
+        return model_architecture_func(seq_length, vocab_size + 1, learning_rate, embedding_dim, best_params["num_heads"], best_params["ffn_units"], dropout_rate)
+
+def load_training_data(encode_dir_path, seq_length):
+    all_input_sequences = []
+    all_target_tokens = []
+    num_files = 10
+
+    for dirpath, _, filenames in os.walk(encode_dir_path):
+        for file in filenames[:num_files]:
+            file_path = os.path.join(dirpath, file)
+            encoded_tokens_list = load_dataset(file_path)
+            if encoded_tokens_list is None:
+                continue
+            for encoded_tokens in encoded_tokens_list:
+                input_sequences, target_tokens = prepare_sequences(encoded_tokens, seq_length=seq_length)
+                all_input_sequences.extend(input_sequences)
+                all_target_tokens.extend(target_tokens)
+
+    return all_input_sequences, all_target_tokens
+
 if __name__ == "__main__":
     main()
+
